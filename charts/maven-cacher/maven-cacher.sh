4603ab1b58624f106cca19ea881910280755510a
Damien Duportal <damien.duportal@gmail.com>
2026-06-24 11:46:41 +0200

    fix(maven-cacher) pin the go-offline dependency to 3.10.0 until https://github.com/jenkins-infra/helpdesk/issues/5198 is fixed
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- charts/maven-cacher/maven-cacher.sh
+++ charts/maven-cacher/maven-cacher.sh
@@ -5,6 +5,10 @@ mvn_cache_dir="${MVN_CACHE_DIR:-"/tmp"}"
 test -d "${mvn_cache_dir}"
 mvn_local_repo="${MVN_LOCAL_REPO:-"${HOME}/.m2/repository"}"
 mvn_cache_archive="${mvn_cache_dir}/maven-bom-local-repo.tar.gz"
+# As 'dependency:go-offline' always use latest and until https://github.com/jenkins-infra/helpdesk/issues/5198 is fixed let's pin to 3.10.0
+mvn_dependency_offline_target='org.apache.maven.plugins:maven-dependency-plugin:3.10.0:go-offline'
+
+MVN_OPTS=("-Dmaven.repo.local=${mvn_local_repo}" "${mvn_dependency_offline_target}")
 
 # Set up local working directory with BOM code
 test -d ./bom || git clone https://github.com/jenkinsci/bom
@@ -12,14 +16,12 @@ pushd ./bom
 
 read -r -a build_lines <<< "${RELEASE_LINES:-}"
 
-# Load dependencies in the local Maven repo
+read -r -a build_lines <<< "${RELEASE_LINES:-}"
 for build_line in "${build_lines[@]}"; do
-  MVN_OPTS=("-Dmaven.repo.local=${mvn_local_repo}")
   if [[ $build_line != "weekly" ]]; then
       MVN_OPTS+=("-P $build_line")
   fi
-  time mvn -pl sample-plugin\
-      dependency:go-offline \
+  time mvn -pl sample-plugin \
       -DincludeScore=runtime,compile,test \
       "${MVN_OPTS[@]}"
 done
